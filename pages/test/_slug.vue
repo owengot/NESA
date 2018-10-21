19babcfc87d6e3e15eb37bc34d73cc538d0e9768
Owen <owengot@gmail.com>
2018-10-21 15:38:35 +0300

    T4 Commit

---
+++ pages/test/_slug.vue
@@ -0,0 +1,34 @@
+<template>
+  <article>
+    <h1>{{ title }}</h1>
+    <p>{{ body }}
+      <br>
+      <strong>Pposted on <time :datetime="date">{{ date }}</time></strong>
+    </p>
+    <img :src="thumbnail">
+  </article>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      body: '',
+      thumbnail: '',
+      date: ''
+    }
+  },
+  async asyncData({ params }) {
+    // const postPromise = process.BROWSER_BUILD
+    //   ? import('~/content/test/posts/' + params.slug + '.json')
+    //   : Promise.resolve(
+    //       require('~/content/test/posts/' + params.slug + '.json')
+    //     );
+
+    let post = await import('~/content/test/posts/' + params.slug + '.json');
+    return post;
+  }
+};
+</script>
+
