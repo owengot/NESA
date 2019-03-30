90e0bbacdf8183d64d03ce5b3fc824316aea0b58
Owen <owengot@gmail.com>
2019-03-30 17:15:08 +0200

    v3.3

--- pages/contact.vue
+++ pages/contact.vue
@@ -19,12 +19,9 @@
                   <input name="bot-field">
                 </label>
               </p>
-              <label class="form-label" for="name">Name:</label>
-              <input class="form-field" name="name" id="name">
-              <label class="form-label" for="email">Email:</label>
-              <input class="form-field" name="email" id="email">
-              <label class="form-label" for="message">Message:</label>
-              <textarea class="form-field" name="message" id="message"></textarea>
+              <input class="form-field" placeholder="Your name" name="name" id="name">
+              <input class="form-field" placeholder="Your email" name="email" id="email">
+              <textarea class="form-field" placeholder="Your message" name="message" id="message"></textarea>
               <input class="form-button" type="submit" value="Send message">
             </form>
           </div>
